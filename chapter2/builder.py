ecae98eb0d8419dcf3d1d2767059bfb4c491e24d
myt2000 <myt2000@126.com>
2019-09-22 23:25:04 +0800

    Enum的用法和建造者模式的用例

--- chapter2/builder.py
+++ chapter2/builder.py
@@ -6,6 +6,16 @@ PizzaDough = Enum('PizzaDough', 'thin thick')
 PizzaSauce = Enum('PizzaSauce', 'tomato creme_fraiche')
 PizzaTopping = Enum('PizzaTopping', 'mozzarella double_mozzarella bacon ham mushrooms red_onion oregano')
 STEP_DELAY = 3
+'''
+>>> from enum import Enum
+>>> a = Enum('test', 'hello world')
+>>> a.hello
+<test.hello: 1>
+>>> a.world
+<test.world: 2>
+>>> print(a.world)
+test.world
+'''
 
 class Pizza:
 
@@ -33,16 +43,22 @@ class MargaritaBuilder:
         self.baking_time = 5
 
     def prepare_dough(self):
+        # 准备面团
+        # PizzaProgress = Enum('PizzaProgress', 'queued preparation baking ready')
+        # self.progress = Enum{name='preparation', value=2}
         self.progress = PizzaProgress.preparation
+        # PizzaDough = Enum('PizzaDough', 'thin thick')
         self.pizza.prepare_dough(PizzaDough.thin)
 
     def add_sauce(self):
+        # 添加调味酱
         print('adding the tomato sauce to your margarita...')
         self.pizza.sauce = PizzaSauce.tomato
         time.sleep(STEP_DELAY)
         print('done with the tomato sauce')
 
     def add_topping(self):
+        # 配料
         print('adding the topping (double mozzarella, oregano) to your margarita')
         self.pizza.topping.append([i for i in
                                    (PizzaTopping.double_mozzarella, PizzaTopping.oregano)])
@@ -50,7 +66,9 @@ class MargaritaBuilder:
         print('done with the topping (double mozzarrella, oregano)')
 
     def bake(self):
+        # 烘烤
         self.progress = PizzaProgress.baking
+        # self.bakint_time = 5
         print('baking your margarita for {} seconds'.format(self.baking_time))
         time.sleep(self.baking_time)
         self.progress = PizzaProgress.ready
@@ -97,6 +115,7 @@ class Waiter:
         self.builder = None
 
     def construct_pizza(self, builder):
+        # builder是两个建造者实例化模型
         self.builder = builder
         [step() for step in (builder.prepare_dough,
                              builder.add_sauce, builder.add_topping, builder.bake)]
@@ -106,9 +125,16 @@ class Waiter:
         return self.builder.pizza
 
 
-def validata_style(builders):
+def validata_style(builders : dict):
+    '''
+
+    :param builders: 是个字典
+    :return:
+    '''
     try:
+        # 判断输入的是m或者c
         pizza_style = input('What pizza would you like, [m]argarita or [c]reamy bacon? ')
+        # 根据输入参数进行实例化建造者MargaritaBuilder，CreamyBaconBuilder
         builder = builders[pizza_style]()
         valid_input = True
     except KeyError as err:
@@ -117,11 +143,14 @@ def validata_style(builders):
     return (True, builder)
 
 def main():
+    # 将两个建造者放入字典中
     builders = dict(m=MargaritaBuilder, c=CreamyBaconBuilder)
     valid_input = False
     while not valid_input:
+        # 传入字典
         valid_input, builder = validata_style(builders)
     print()
+    # builder是CreamyBaconBuilder()或者MargaritaBuilder()
     waiter = Waiter()
     waiter.construct_pizza(builder)
     pizza = waiter.pizza
